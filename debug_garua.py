250807a52cb4a689fe9ab189f7aa65a717f599b1
Israel9974 <Israelcornejozapata@gmail.com>
2026-09-07 21:20:36 -0500

    Update debug_garua.py

--- debug_garua.py
+++ debug_garua.py
@@ -1,8 +1,21 @@
 import logging
-import sys
+logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(name)s:%(message)s")
+import sys
+from zendriver import Config
+from garua.exceptions import BrowserNotFoundError
+import garua.scraping.browser as browser_mod
+import garua.scraping.scraper as scraper_mod
+
+def patched_get_browser_config():
+    check = browser_mod.check_browser()
+    if not check.ok or not check.path:
+        raise BrowserNotFoundError(check.message)
+    return Config(browser_executable_path=check.path, sandbox=False)
+
+# Reemplazamos la función en ambos lugares donde Garúa la usa
+browser_mod.get_browser_config = patched_get_browser_config
+scraper_mod.get_browser_config = patched_get_browser_config
 
 from garua.main import cli
-
 sys.exit(cli())
